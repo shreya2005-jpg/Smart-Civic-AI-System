5d4cf7261ac4c5943ac3dc8060860eb189b18841
shreya2005-jpg <sn73patil@gmail.com>
2026-06-23 19:59:27 +0530

    Update location.py

--- backend/location.py
+++ backend/location.py
@@ -1,1 +1,10 @@
 
+import re
+
+def extract_location(text):
+    match = re.search(r"near (.*)|in (.*)", text)
+
+    if match:
+        return match.group(1) or match.group(2)
+
+    return "Unknown Location"
