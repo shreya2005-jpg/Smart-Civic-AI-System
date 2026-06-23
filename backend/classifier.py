bfb80aa55c05f95acd5cddb36148af3e149a7960
shreya2005-jpg <sn73patil@gmail.com>
2026-06-23 19:56:34 +0530

    Update classifier.py

--- backend/classifier.py
+++ backend/classifier.py
@@ -1,1 +1,20 @@
 
+from transformers import pipeline
+
+# AI model for text classification
+classifier = pipeline(
+    "zero-shot-classification",
+    model="facebook/bart-large-mnli"
+)
+
+labels = [
+    "garbage issue",
+    "pothole",
+    "water problem",
+    "electricity issue",
+    "drainage issue"
+]
+
+def classify(text):
+    result = classifier(text, labels)
+    return result["labels"][0]
